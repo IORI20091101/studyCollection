f3cb98c9d6d18205e6374f82e9ecf1e154d0fa44
sundongzhi <sundongzhi11014111@126.com>
2015-02-03 18:28:43 +0800

    add chat

---
+++ node/chat/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+
+var server = require('http').Server(app);
+
+var io = require('socket.io')(server);
+
+server.listen(3030);
+
+app.use('/static', express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.redirect('/static/index.html');
+  //res.sendfile(__dirname+'/public/index.html');
+});
+
+
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('anotherNews', function (data) {
+    console.log(data);
+  });
+});
+
