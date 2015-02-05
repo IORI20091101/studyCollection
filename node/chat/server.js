91d7afcb529cf029a1a5b9eb80b9af46c0982694
sundongzhi <sundongzhi11014111@126.com>
2015-02-05 18:53:14 +0800

    chat

--- node/chat/server.js
+++ node/chat/server.js
@@ -8,11 +8,23 @@ var io = require('socket.io')(server);
 
 server.listen(3030);
 
+
+var path = require('path');
+
 app.use('/static', express.static(__dirname + '/public'));
 
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'jade');
+
+
 app.get('/', function (req, res) {
-  res.redirect('/static/index.html');
+  //res.redirect('/static/index.html');
   //res.sendfile(__dirname+'/public/index.html');
+  var view = 'index';
+  res.render(view, {
+
+  });
 });
 
 
