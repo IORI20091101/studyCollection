56b95f717a779bab7b3b6b3be9fc55e05799194f
孙东芝 <sundongzhi11014111@126.com>
2015-06-29 22:20:18 +0800

    update app.js

--- node/mongodb/app.js
+++ node/mongodb/app.js
@@ -16,45 +16,6 @@ app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.set('views',__dirname + '/views');
 
-app.get('/', function(req, res) {
-    res.render('index', {authenticated: false});
-});
-
-app.get('/login', function(req, res) {
-    res.render('login');
-});
-
-
-app.get('/login/:signupEmail', function(req, res) {
-    res.render('login', {signupEmail: req.params.signupEmail});
-});
-
-app.get('/signup', function(req, res) {
-    res.render('signup');
-});
-
-app.post('/login', function(req, res, next) {
-    console.log(req.body.user.email);
-    console.log(req.body.user.password);
-    var logDoc = app.users.findOne({email: req.body.user.email,password: req.body.user.passord});
-    if(!logDoc) {
-        return res.send('<p>User not found, Go back and try again</p>');
-    }
-    console.log(logDoc);
-    //req.session.loggedIn = doc._id.toString();
-    res.redirect('/');
-});
-
-app.post('/signup', function(req, res, next) {
-    var user = req.body.user;
-    app.users.insert(user, function(err, doc) {
-        if(err) return next(err);
-        console.log("注册请求， 向表中插入新注册的user");
-        res.redirect('/login/' + doc.ops[0].email);
-    })
-});
-
-
 
 MongoClient.connect(url, function(err, db) {
     if(err) throw err;
@@ -90,7 +51,82 @@ MongoClient.connect(url, function(err, db) {
 
 app.listen(3030, function() {
         console.log('\033[96m app listening on *:3030');
+});
+
+
+
+//中间件
+app.use(function(req, res, next) {
+    if(req.session.loggedIn) {
+        res.locals('authenticated', true);
+        app.users.findOne({_id: {$oid: req.session.loggedIn}},function(err, results) {
+            if( err ) return next(err);
+            res.local('me',results);
+            next();
+        })
+    } else {
+        res.locals('authenticated', false);
+        next();
+    }
+
+});
+
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+
+app.get('/login/:signupEmail', function(req, res) {
+    res.render('login', {signupEmail: req.params.signupEmail});
+});
+
+app.get('/signup', function(req, res) {
+    res.render('signup');
+});
+
+app.post('/login', function(req, res, next) {
+    var email = req.body.user.email;
+    var password = req.body.user.password;
+    console.log(email);
+    console.log(password);
+
+
+    app.users.findOne({email:email,password:password}, function(err, results) {
+        if (err) return next(err);
+        if(!results) {
+            return res.send('<p>User not found, Go back and try again</p>');
+        }
+        console.log(results)
+        req.session.loggedIn = results._id.toString();
+        res.redirect('/');
     });
+
+});
+
+app.post('/signup', function(req, res, next) {
+    var user = req.body.user;
+    app.users.insert(user, function(err, doc) {
+        if(err) return next(err);
+        console.log("注册请求， 向表中插入新注册的user");
+        res.redirect('/login/' + doc.ops[0].email);
+    })
+});
+
+app.get('/logout', function(req, res, next) {
+    req.session.loggedIn = null;
+    //req.session.regenerate();   彻底清除session
+    res.redirect('/');
+});
+
+
+
+
+
 
 /*
 //old version
