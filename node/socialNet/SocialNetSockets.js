4f4c86ea6863465112de11c64226b29e98e5ecef
IORI20091101 <sundongzhi11014111@126.com>
2015-08-04 18:26:55 +0800

    update socialnet

--- node/socialNet/SocialNetSockets.js
+++ node/socialNet/SocialNetSockets.js
@@ -1,19 +1,43 @@
-define(['Sockets'], function(sio) {
-    var socket = null;
-    var initialize = function(eventDispatcher) {
-        eventDispatcher.bind('app:loggedin', connectSocket);
+define(['Sockets','models/contactCollection', 'views/chat'], function(sio, contactCollection, chatView) {
+    var SocialNetSockets = function(eventDispatcher) {
+        var socket = null;
+        var connectSocket = function() {
+            socket = io.connect();
+            socket.on('connect_failed', function(reason) {
+                console.error('unable to connect', reason);
+            }).on('connect', function() {
+                console.info('successfully established a connection');
+                eventDispatcher.bind('socket:chat',sendChat);
+                socket.on('chatserver', function(data) {
+                    eventDispatcher.trigger('socket:chat:start:' + data.from);
+                    eventDispatcher.trigger('scoket:chat:in:' + data.from, data);
+                });
+
+                var contactCollection = new ContactCollection();
+                contactCollection.url='/accounts/me/contacts';
+                new ChatView({
+                    collection: contactsCollection,
+                    socketEvents: eventDispatcher
+                }).render();
+                contactCollection.fetch();
+            });
+        }
+
+        var sendChat = function(payload){
+            if( null != socket ) {
+                socket.emit('chatclient', payload);
+            }
+        }
+
+        eventDispatcher.bind('app:loggedin', connectSocket)；
     }
 
-    var connectSocket = function() {
-        socket = io.connect().socket;
-        socket.on('connect_failed', function(reason) {
-            console.error('unable to connect', reason);
-        }).on('connect', function() {
-            console.info('successfully established a connection');
-        });
-    }
+
+
 
     return {
-        initialize: initialize
+        initialize: function(eventDispatcher) {
+            SocialNetSockets(eventDispatcher);
+        }
     }
 });
