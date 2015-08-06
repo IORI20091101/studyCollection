72f5f13b30e3ec164a9c571fec59130a90312886
孙东芝 <sundongzhi11014111@126.com>
2015-08-06 21:07:45 +0800

    update

--- node/socialNet/config/mail.js
+++ node/socialNet/config/mail.js
@@ -4,5 +4,10 @@ module.export = {
     auth: {
         user: 'sundongzhi11014111@126.com',
         pass: 'xxxxx'
-    }
+    },
+    port: 587,
+    secureConnection: false,
+    ignoreTLS: false,
+    debug: false,
+    maxConnections: 5 // Default is 5
 }
