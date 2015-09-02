25d97f95bb820a952beaa652f7b073d316ebe860
dongzhi <sundongzhi11014111@126.com>
2015-09-02 18:13:27 +0800

    update

--- node/logipCount/app.js
+++ node/logipCount/app.js
@@ -6,21 +6,37 @@ var fs = require('fs');
 
 var readline = require("readline");
 
+
+var program = require('commander');
+
+program
+  .version('0.0.1');
+
+// must be before .parse() since
+// node's emit() is immediate
+
+program.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ node app /Users/toshiba/Downloads/www.ezhe.com.log-20150811');
+  console.log('');
+  console.log('');
+}).parse(process.argv);
+
+if (!program.args.length) program.help();
+
+
+
 var argements = process.argv.splice(2)[0];
 console.log(argements);
 
+var config = require('./config');
 
 var readStream = fs.createReadStream(argements); //"/Users/toshiba/Downloads/www.ezhe.com.log-20150811"
 
 
 var mysql = require('mysql');
-var pool = mysql.createPool({
-    host:'172.16.0.254',
-    user:'root',
-    password:'gozapdev',
-    database:'ehaitao',
-    port: 4300
-});
+var pool = mysql.createPool(config);
 
 
 
@@ -37,164 +53,349 @@ readStream.on("end", function() {
         log2data(line)
     });
 
-    console.log('Have a good day!~~~');
+    var ipDataSort = _.groupBy(ipData, function(v) {
+        return v.dateFinal;
+    });
 
-    var groupData = _.groupBy(ipData, function(val) {
-        return val.web;
+    _.each(ipDataSort, function(j,l) {
+        var groupData = _.groupBy(j, function(val) {
+            return val.web;
+        })
+        //console.log(groupData["www.ezhe.com"]);
+
+        var utcdate = l;
+        var total = "";
+        var uniqtotal = "";
+        var platform = "";
+
+        var wwwPV = 0;
+        var apiPV = 0;
+        var wapPV = 0;
+
+        var wwwUV = 0;
+        var apiUV = 0;
+        var wapUV = 0;
+
+        var flag1 = false;
+        var flag2 = false;
+
+        console.log('Have a good day!~~~');
+
+
+
+        _.each(groupData, function(val, key) {
+            total = val.length;
+            uniqtotal = _.uniq(val, function(v) {
+                return v.ip;
+            });
+            platform = key;
+            if( key == "www.ezhe.com" || "dohko.www.ezhe.com" ) {
+                wwwPV = val.length;
+                wwwUV = uniqtotal.length;
+
+            } else if( key == "m.ezhe.com" || "dohko.m.ezhe.com") {
+                wapPV = val.length;
+                wapUV = uniqtotal.length;
+
+            } else if( key == "api.ezhe.com" || "dohko.api.ezhe.com" ) {
+                apiPV = val.length;
+                apiUV = uniqtotal.length;
+
+            }
+
+
+        });
+
+        pool.getConnection(function(err, conn) {
+            if( err ) console.log("POOL ==>" + err);
+
+
+
+            conn.query('select * from tbl_ip_statistics where date = "'+ l +'"',[] ,function(err, results) {
+                if( err ) console.log(err);
+
+
+                if( results && results.length == 0 ) {
+                    var selectSQL = 'insert into tbl_ip_statistics(date, wwwPV, apiPV, wapPV, wwwUV, apiUV, wapUV ) values(?,?,?,?,?,?,?)';
+
+                    pool.getConnection(function(err, conn) {
+                        if( err ) console.log("POOL ==>" + err);
+
+
+
+                        conn.query(selectSQL,[ l, wwwPV, apiPV, wapPV, wwwUV, apiUV, wapUV ] ,function(err, rows) {
+                            if( err ) console.log(err);
+
+                            if( rows.affectedRows == 1 ) {
+                                console.log("插入数据成功！");
+                            }
+
+                            conn.release();
+                        });
+
+                    });
+                } else {
+
+                    var resulsData  = results[0];
+                    wwwPV += resulsData.wwwPV;
+                    apiPV += resulsData.apiPV;
+                    wapPV += resulsData.wapPV;
+                    wwwUV += resulsData.wwwUV;
+                    apiUV += resulsData.apiUV;
+                    wapUV += resulsData.wapUV;
+
+                    var updateSQL = 'update tbl_ip_statistics set wwwPV=?, apiPV=?, wapPV=?, wwwUV=?, apiUV= ?, wapUV= ? where id= ' + resulsData.id;
+                    pool.getConnection(function(err, conn) {
+                        if( err ) console.log("POOL ==>" + err);
+
+
+                        conn.query(updateSQL,[  wwwPV, apiPV, wapPV, wwwUV, apiUV, wapUV ] ,function(err, rows) {
+                            if( err ) console.log(err);
+
+                                if( rows.changedRows > 0 ) {
+                                    console.log("更新数据成功！");
+
+                                }
+
+                            conn.release();
+                        });
+
+                    });
+                }
+
+
+                conn.release();
+            });
+
+        });
+
+
+        var hourData =_.groupBy(j, function(item, i) {
+            var str = Number(item.dateStr.split(':')[1]);
+            return str;
+        });
+
+
+        var indexArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+
+        _.each(indexArr,function(v, k) {
+            if(  hourData[k] == null || !hourData[k] ) {
+                hourData[k] = [];
+            }
+        });
+
+
+
+
+        pool.getConnection(function(err, conn) {
+            if( err ) console.log("POOL ==>" + err);
+
+            conn.query('select * from tbl_ip_time_statisitcs where date = "'+ l +'"',[] ,function(err, results) {
+                if( err ) console.log(err);
+
+
+                if( results && results.length == 0 ) {
+                    var insertSql = 'insert into tbl_ip_time_statisitcs('+
+                        'date,'+
+                        ' hour1,'+
+                        ' hour2,'+
+                        ' hour3,'+
+                        ' hour4,'+
+                        ' hour5,'+
+                        ' hour6,'+
+                        ' hour7,'+
+                        ' hour8,'+
+                        ' hour9,'+
+                        ' hour10,'+
+                        ' hour11,'+
+                        ' hour12,'+
+                        ' hour13,'+
+                        ' hour14,'+
+                        ' hour15,'+
+                        ' hour16,'+
+                        ' hour17,'+
+                        ' hour18,'+
+                        ' hour19,'+
+                        ' hour20,'+
+                        ' hour21,'+
+                        ' hour22,'+
+                        ' hour23,'+
+                        ' hour24'+
+                        ' ) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+
+
+                    pool.getConnection(function(err, conn) {
+                        if( err ) console.log("POOL ==>" + err);
+
+                        conn.query(insertSql,[
+                            l,
+                            hourData[0].length,
+                            hourData[1].length,
+                            hourData[2].length,
+                            hourData[3].length,
+                            hourData[4].length,
+                            hourData[5].length,
+                            hourData[6].length,
+                            hourData[7].length,
+                            hourData[8].length,
+                            hourData[9].length,
+                            hourData[10].length,
+                            hourData[11].length,
+                            hourData[12].length,
+                            hourData[13].length,
+                            hourData[14].length,
+                            hourData[15].length,
+                            hourData[16].length,
+                            hourData[17].length,
+                            hourData[18].length,
+                            hourData[19].length,
+                            hourData[20].length,
+                            hourData[21].length,
+                            hourData[22].length,
+                            hourData[23].length
+                           ] ,function(err, rows) {
+                            if( err ) console.log(err);
+                            if( rows.affectedRows == 1 ) {
+                                console.log("插入数据成功！");
+                            }
+                            conn.release();
+                        });
+
+                    });
+                } else {
+
+                    var resData  = results[0];
+                    var hour1 = hourData[0].length + resData.hour1;
+                    var hour2 = hourData[1].length + resData.hour2;
+                    var hour3 = hourData[2].length + resData.hour3;
+                    var hour4 = hourData[3].length + resData.hour4;
+                    var hour5 = hourData[4].length + resData.hour5;
+                    var hour6 = hourData[5].length + resData.hour6;
+                    var hour7 = hourData[6].length + resData.hour7;
+                    var hour8 = hourData[7].length + resData.hour8;
+                    var hour9 = hourData[8].length + resData.hour9;
+                    var hour10 = hourData[9].length + resData.hour10;
+                    var hour11 = hourData[10].length + resData.hour11;
+                    var hour12 = hourData[11].length + resData.hour12;
+                    var hour13 = hourData[12].length + resData.hour13;
+                    var hour14 = hourData[13].length + resData.hour14;
+                    var hour15 = hourData[14].length + resData.hour15;
+                    var hour16 = hourData[15].length + resData.hour16;
+                    var hour17 = hourData[16].length + resData.hour17;
+                    var hour18 = hourData[17].length + resData.hour18;
+                    var hour19 = hourData[18].length + resData.hour19;
+                    var hour20 = hourData[19].length + resData.hour20;
+                    var hour21 = hourData[20].length + resData.hour21;
+                    var hour22 = hourData[21].length + resData.hour22;
+                    var hour23 = hourData[22].length + resData.hour23;
+                    var hour24 = hourData[23].length + resData.hour24;
+
+
+                    var updateSQL = 'update tbl_ip_time_statisitcs set hour1 = ?,' +
+                                                                       'hour2 = ?,' +
+                                                                       'hour3 = ?,' +
+                                                                       'hour4 = ?,' +
+                                                                       'hour5 = ?,' +
+                                                                       'hour6 = ?,' +
+                                                                       'hour7 = ?,' +
+                                                                       'hour8 = ?,' +
+                                                                       'hour9 = ?,' +
+                                                                       'hour10 = ?,' +
+                                                                       'hour11 = ?,' +
+                                                                       'hour12 = ?,' +
+                                                                       'hour13 = ?,' +
+                                                                       'hour14 = ?,' +
+                                                                       'hour15 = ?,' +
+                                                                       'hour16 = ?,' +
+                                                                       'hour17 = ?,' +
+                                                                       'hour18 = ?,' +
+                                                                       'hour19 = ?,' +
+                                                                       'hour20 = ?,' +
+                                                                       'hour21 = ?,' +
+                                                                       'hour22 = ?,' +
+                                                                       'hour23 = ?,' +
+                                                                       'hour24 = ? ' +
+                                                                       'where id= ' +resData.id;
+
+
+                    pool.getConnection(function(err, conn) {
+                        if( err ) console.log("POOL ==>" + err);
+
+
+                        conn.query(updateSQL,[
+                            hour1,
+                            hour2,
+                            hour3,
+                            hour4,
+                            hour5,
+                            hour6,
+                            hour7,
+                            hour8,
+                            hour9,
+                            hour10,
+                            hour11,
+                            hour12,
+                            hour13,
+                            hour14,
+                            hour15,
+                            hour16,
+                            hour17,
+                            hour18,
+                            hour19,
+                            hour20,
+                            hour21,
+                            hour22,
+                            hour23,
+                            hour24
+                        ] ,function(err, rows) {
+                            if( err ) console.log(err);
+
+                            if( rows.changedRows > 0 ) {
+                                console.log("更新数据成功！");
+
+                            }
+
+
+
+
+                            conn.release();
+                        });
+
+                    });
+                }
+
+
+                conn.release();
+            });
+
+        });
+
+
     })
-    //console.log(groupData["www.ezhe.com"]);
-
-    var utcdate = new Date().toLocaleDateString();
-    var total = "";
-    var uniqtotal = "";
-    var platform = "";
-
-    var wwwPV = 0;
-    var apiPV = 0;
-    var wapPV = 0;
-
-    var wwwUV = 0;
-    var apiUV = 0;
-    var wapUV = 0;
-    _.each(groupData, function(val, key) {
-        total = val.length;
-        uniqtotal = _.uniq(val, function(v) {
-            return v.ip;
-        });
-        platform = key;
-        if( key == "www.ezhe.com" || "dohko.www.ezhe.com" ) {
-            wwwPV = val.length;
-            wwwUV = uniqtotal.length;
-
-        } else if( key == "m.ezhe.com" || "dohko.m.ezhe.com") {
-            wapPV = val.length;
-            apiUV = uniqtotal.length;
-
-        } else if( key == "api.ezhe.com" || "dohko.api.ezhe.com" ) {
-            apiPV = val.length;
-            wapUV = uniqtotal.length;
-
-        }
-
-
-    });
-
-    var selectSQL = 'insert into tbl_ip_statistics(date, wwwPV, apiPV, wapPV, wwwUV, apiUV, wapUV ) values(?,?,?,?,?,?,?)';
-    //连接池，已经解决了自动重连的问题了，后面我们的开发，可以尽量使用pooling的方式。
-    pool.getConnection(function(err, conn) {
-        if( err ) console.log("POOL ==>" + err);
-
-
-
-        conn.query(selectSQL,[ utcdate, wwwPV, apiPV, wapPV, wwwUV, apiUV, wapUV ] ,function(err, rows) {
-            if( err ) console.log(err);
-
-            if( rows.affectedRows == 1 ) {
-                console.log("插入数据成功！");
-
-            }
-
-
-            conn.release();
-        });
-
-    });
-
-
-
-
-    var hourData =_.groupBy(ipData, function(item, i) {
-        var str = Number(item.dateStr.split(':')[1]);
-        return str;
-    });
-
-
-    var indexArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
-
-    _.each(indexArr,function(v, k) {
-        if(  hourData[k] == null || !hourData[k] ) {
-            hourData[k] = [];
-        }
-    });
-
-
-
-
-
-    var insertSql = 'insert into tbl_ip_time_statisitcs('+
-        'date,'+
-        ' hour1,'+
-        ' hour2,'+
-        ' hour3,'+
-        ' hour4,'+
-        ' hour5,'+
-        ' hour6,'+
-        ' hour7,'+
-        ' hour8,'+
-        ' hour9,'+
-        ' hour10,'+
-        ' hour11,'+
-        ' hour12,'+
-        ' hour13,'+
-        ' hour14,'+
-        ' hour15,'+
-        ' hour16,'+
-        ' hour17,'+
-        ' hour18,'+
-        ' hour19,'+
-        ' hour20,'+
-        ' hour21,'+
-        ' hour22,'+
-        ' hour23,'+
-        ' hour24'+
-        ' ) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
-    //连接池，已经解决了自动重连的问题了，后面我们的开发，可以尽量使用pooling的方式。
-    pool.getConnection(function(err, conn) {
-        if( err ) console.log("POOL ==>" + err);
-
-        conn.query(insertSql,[
-            utcdate,
-            hourData[0].length,
-            hourData[1].length,
-            hourData[2].length,
-            hourData[3].length,
-            hourData[4].length,
-            hourData[5].length,
-            hourData[6].length,
-            hourData[7].length,
-            hourData[8].length,
-            hourData[9].length,
-            hourData[10].length,
-            hourData[11].length,
-            hourData[12].length,
-            hourData[13].length,
-            hourData[14].length,
-            hourData[15].length,
-            hourData[16].length,
-            hourData[17].length,
-            hourData[18].length,
-            hourData[19].length,
-            hourData[20].length,
-            hourData[21].length,
-            hourData[22].length,
-            hourData[23].length
-           ] ,function(err, rows) {
-            if( err ) console.log(err);
-
-            if( rows.affectedRows == 1 ) {
-                console.log("插入数据成功！");
-
-            }
-
-            conn.release();
-        });
-
-    });
-
-
-
 
 });
+
+
+setTimeout(function() {
+    process.exit(0);
+}, 30000)
+
+var regional = {
+    "january" : "01", "february":"02", "march":"03", "april":"04", "may":"05", "june":"06", "july":"07", "august":"08", "september":"09", "october":"10", "november":"11", "december":"12"
+}
+
+function covertMonth(mon) {
+    var lowMon = mon.toLowerCase();
+    var key = "";
+    _.each(regional, function(v, k) {
+        if( (k).indexOf(lowMon) >=0 ) {
+            key = k;
+            return false;
+        }
+    });
+    return regional[key];
+
+}
 
 function log2data(line) {
 
@@ -205,7 +406,11 @@ function log2data(line) {
         return false;
     } else {
         data.ip = list[0];
+        var dateArr = list[3].substring(1);
         data.dateStr = list[3].substring(1);
+
+        var dateList = dateArr.split(":")[0].split("/");
+        data.dateFinal = dateList[2] + '-' + covertMonth(dateList[1]) + '-' + dateList[0];
         data.web = list[5].substring(1, (list[5].length - 1) );
 
         ipData.push(data);
@@ -214,4 +419,29 @@ function log2data(line) {
 }
 
 
+
+
+
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-f, --foo', 'enable some foo')
+  .option('-b, --bar', 'enable some bar')
+  .option('-B, --baz', 'enable some baz');
+
+// must be before .parse() since
+// node's emit() is immediate
+
+program.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ custom-help --help');
+  console.log('    $ custom-help -h');
+  console.log('');
+});
+
+program.parse(process.argv);
+
+program.help();
 
---
+++ node/logipCount/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host:'172.16.0.254',
+    user:'root',
+    password:'gozapdev',
+    database:'ehaitao',
+    port: 4300
+}
