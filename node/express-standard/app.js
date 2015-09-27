7d9169840a59f0c2b83a60aaa20cd9c98bfbb722
孙东芝 <sundongzhi11014111@126.com>
2015-09-27 20:25:21 +0800

    sds

--- node/express-standard/app.js
+++ node/express-standard/app.js
@@ -48,7 +48,7 @@ app.use(function(req, res, next) {
   var uid = req.session.uid;
   if(!uid) return next();
   User.get(uid, function(err, user) {
-    if(err) return next(err);
+    if(err) return next(err);sdsdsd
     req.user = res.locals.user = user;
     next();
   })
