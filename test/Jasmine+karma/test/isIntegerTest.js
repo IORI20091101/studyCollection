9463ce28ea08a4081da3c82077d55c39357af7a9
孙东芝 <sundongzhi11014111@126.com>
2016-04-06 07:37:09 +0800

    update

---
+++ test/Jasmine+karma/test/isIntegerTest.js
@@ -0,0 +1,11 @@
+describe("this is a integer test!", function() {
+    it("Is integer", function() {
+
+            expect(true).toEqual(isInteger(20));
+
+            expect(false).toEqual(isInteger("20"));
+            expect(false).toEqual(isInteger(0));
+
+
+    });
+});
