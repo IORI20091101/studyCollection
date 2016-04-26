8ac6a0f7ab10f1fb3e656c2d2057b0a116107909
IOR20091101 <sundongzhi11014111@126.com>
2016-04-26 18:58:27 +0800

    update

---
+++ test/yeomanAngularGulp/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('yeomanAngularGulp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ test/yeomanAngularGulp/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('yeomanAngularGulp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
