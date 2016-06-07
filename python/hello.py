d4f3dd08c91929b95cb04f22028393d941d14acd
IOR20091101 <sundongzhi11014111@126.com>
2016-06-07 20:42:35 +0800

    study python

--- python/hello.py
+++ python/hello.py
@@ -3,3 +3,34 @@ print "hello, world!"
 name = raw_input("what is your name? ")
 
 print "hello ," + name + "!"
+
+months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'Doctober',
+    'November',
+    'December'
+]
+
+endings=['st','nd', 'rd'] + 17*['th'] \
+    + ['st', 'nd', 'rd'] + 7*['th'] \
+    +['st']
+
+
+year = raw_input('Year: ')
+month = raw_input('Month: ')
+day = raw_input('day: ')
+
+month_number = int(month)
+day_number = int(day)
+month_name = months[month_number - 1]
+ordinal = day + endings[day_number-1]
+
+print month_name + ' ' + ordinal + ', '+year
