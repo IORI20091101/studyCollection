b4520fb0e3b6166c1c7e50ef809cde8d8a649f9c
IOR20091101 <sundongzhi11014111@126.com>
2016-07-18 18:44:05 +0800

    update

---
+++ python/plan.py
@@ -0,0 +1,11 @@
+girls = ['alic', 'bernice', 'clarice']
+
+boys = ['chris', 'arnold', 'bob']
+
+letterGirls = {}
+
+for girl in girls :
+    letterGirls.setdefault(girl[0], []).append(girl)
+
+
+print [b + '+' + g for b in boys for g in letterGirls[b[0]]]
